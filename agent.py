fd82d3bc0f2e1b6896e5c742065d65cb951b645b
nezuni1812 <tranngocuyennhi1812@gmail.com>
2024-08-05 12:01:31 +0700

    modify receive percept

--- agent.py
+++ agent.py
@@ -1,4 +1,4 @@
-from KnowledgeBase import WumpusKB
+from KnowledgeBase import *
 
 class Agent:
     def __init__(self, interface):
@@ -8,9 +8,19 @@ class Agent:
         self.interface.set_agent_cell(self.current_position)
 
     def perceive_current_cell(self):
-        cell_info = self.interface.get_percepts()
-        self.KB = self.KB.add_clause(cell_info)
-        return cell_info
+        percept_types = self.interface.get_percepts()  # Example: {'P', '~W', '~S',...}
+        x, y = self.current_position
+
+        for percept in percept_types:
+            percept_symbol = symbols(f'{percept}{x}{y}')
+            
+            # Convert percept to its logical representation if it is negative (e.g., '~W' for no Wumpus)
+            if percept.startswith('~'):
+                percept_symbol = Not(percept_symbol)
+            
+            self.KB.add_clause(percept_symbol)
+        
+        return percept_types
     
     def perform_action(self, action):
         self.interface.log_action(action)
